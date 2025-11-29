3af3ba4eaa8c041946fa01c8facf5b728eefda64
Devaj Shourya Bhogireddi <shourya.dsb@gmail.com>
2025-11-29 12:15:11 -0600

    pieces and seperate files

--- main.py
+++ main.py
@@ -2,168 +2,9 @@ import pygame
 from sys import exit
 import time
 
+from pieces import Pawn, Rook, Knight, Bishop, Queen
 
 #https://www.youtube.com/watch?v=AY9MnQ4x3zk
-
-class ChessPiece:
-    imageFilePath = ""
-    position = (0, 0)
-    color = ""
-    moveList = []
-
-
-class Pawn(ChessPiece):
-    imageFilePath = "images/pawn.png"
-    
-
-    def __init__(self, position, color):
-        self.position = position
-        self.color = color
-        self.generateMoveList()
-        self.surface = pygame.Surface((50, 50))
-        self.surface.fill((0, 100, 0))  # Placeholder for pawn image
-    
-    def generateMoveList(self):
-        self.moveList = []
-        x, y = self.position
-        direction = 1 if self.color == "white" else -1
-
-        if chessBoard[x][y + direction] is None:
-            self.moveList.append((x, y + direction))
-
-        if (self.color == "white" and y == 1) or (self.color == "black" and y == 6):
-            self.moveList.append((x, y + 2 * direction))
-        
-        try:
-            if chessBoard[x - 1][y + direction] is not None:
-                self.moveList.append((x - 1, y + direction))
-        except IndexError:
-            pass
-        try: 
-            if chessBoard[x + 1][y + direction] is not None:
-                self.moveList.append((x + 1, y + direction))
-        except IndexError:
-            pass
-        
-        ml = self.moveList.copy()
-        for i in ml:
-            if chessBoard[i[0]][i[1]] is not None:
-                if chessBoard[i[0]][i[1]].color == self.color:
-                    self.moveList.remove(i)
-        
-
-    def move(self, newPosition):
-        if newPosition in self.moveList:
-            self.position = newPosition
-            self.generateMoveList()
-        else:
-            raise ValueError("Invalid move for Pawn")
-        
-
-
-class Rook(ChessPiece):
-    imageFilePath = "images/rook.png"
-    
-    def __init__(self, position, color):
-        self.position = position
-        self.color = color
-        self.generateMoveList()
-        self.surface = pygame.Surface((50, 50))
-        self.surface.fill((100, 0, 0))  # Placeholder for rook image
-    
-    def generateMoveList(self):
-        self.moveList = []
-        x, y = self.position
-
-        for i in range(x, 8):
-            if i != x:
-                self.moveList.append((i, y))
-                if chessBoard[i][y] is not None:
-                    break
-        for i in range(x, -1, -1):
-            if i != x:
-                self.moveList.append((i, y))
-                if chessBoard[i][y] is not None:
-                    break
-        for i in range(y, 8):
-            if i != y:
-                self.moveList.append((x, i))
-                if chessBoard[x][i] is not None:
-                    break
-        for i in range(y, -1, -1):
-            if i != y:
-                self.moveList.append((x, i))
-                if chessBoard[x][i] is not None:
-                   break
-
-        ml = self.moveList.copy()
-        for i in ml:
-            if chessBoard[i[0]][i[1]] is not None:
-                if chessBoard[i[0]][i[1]].color == self.color:
-                    self.moveList.remove(i)
-
-    def move(self, newPosition):
-        if newPosition in self.moveList:
-            self.position = newPosition
-            self.generateMoveList()
-        else:
-            raise ValueError("Invalid move for Rook")
-        
-
-class Bishop(ChessPiece):
-    imageFilePath = "images/bishop.png"
-    # Implementation similar to Pawn and Rook
-    def __init__(self, position, color):
-        self.position = position
-        self.color = color
-        self.generateMoveList()
-        self.surface = pygame.Surface((50, 50))
-        self.surface.fill((0, 0, 100))  # Placeholder for bishop image
-    def generateMoveList(self):
-        self.moveList = []
-        x, y = self.position
-
-        # Diagonal moves
-        for i in range(1, 8):
-            if x + i < 8 and y + i < 8:
-                self.moveList.append((x + i, y + i))
-                if chessBoard[x + i][y + i] is not None:
-                    break
-        for i in range(1, 8):
-            if x - i >= 0 and y + i < 8:
-                self.moveList.append((x - i, y + i))
-                if chessBoard[x - i][y + i] is not None:
-                    break
-        for i in range(1, 8):
-            if x + i < 8 and y - i >= 0:
-                self.moveList.append((x + i, y - i))
-                if chessBoard[x + i][y - i] is not None:
-                    break
-        for i in range(1, 8):
-            if x - i >= 0 and y - i >= 0:
-                self.moveList.append((x - i, y - i))
-                if chessBoard[x - i][y - i] is not None:
-                    break
-
-        ml = self.moveList.copy()
-        for i in ml:
-            if chessBoard[i[0]][i[1]] is not None:
-                if chessBoard[i[0]][i[1]].color == self.color:
-                    self.moveList.remove(i)
-    def move(self, newPosition):
-        if newPosition in self.moveList:
-            self.position = newPosition
-            self.generateMoveList()
-        else:
-            raise ValueError("Invalid move for Bishop")
-
-        
-
-
-
-
-
-
 
 
 pygame.init()
@@ -177,26 +18,28 @@ turn = 0  # 0 for white's turn, 1 for black
 
 chessBoard = [[None for _ in range(8)] for _ in range(8)]
 
-
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Chess Game")
 clock = pygame.time.Clock()
 
 
 for i in range(8):
-    chessBoard[i][1] = Pawn((i, 1), "white")
-    chessBoard[i][6] = Pawn((i, 6), "black")
-chessBoard[0][0] = Rook((0, 0), "white")
-chessBoard[7][0] = Rook((7, 0), "white")
-chessBoard[0][7] = Rook((0, 7), "black")
-chessBoard[7][7] = Rook((7, 7), "black")
-chessBoard[2][0] = Bishop((2, 0), "white")
-chessBoard[5][0] = Bishop((5, 0), "white")
-chessBoard[2][7] = Bishop((2, 7), "black")
-chessBoard[5][7] = Bishop((5, 7), "black")
-
-
-
+    chessBoard[i][1] = Pawn((i, 1), "white", chessBoard)
+    chessBoard[i][6] = Pawn((i, 6), "black", chessBoard)
+chessBoard[0][0] = Rook((0, 0), "white", chessBoard)
+chessBoard[7][0] = Rook((7, 0), "white", chessBoard)
+chessBoard[0][7] = Rook((0, 7), "black", chessBoard)
+chessBoard[7][7] = Rook((7, 7), "black", chessBoard)
+chessBoard[2][0] = Bishop((2, 0), "white", chessBoard)
+chessBoard[5][0] = Bishop((5, 0), "white", chessBoard)
+chessBoard[2][7] = Bishop((2, 7), "black", chessBoard)
+chessBoard[5][7] = Bishop((5, 7), "black", chessBoard)
+chessBoard[1][0] = Knight((1, 0), "white", chessBoard)
+chessBoard[6][0] = Knight((6, 0), "white", chessBoard)
+chessBoard[1][7] = Knight((1, 7), "black", chessBoard)
+chessBoard[6][7] = Knight((6, 7), "black", chessBoard)
+chessBoard[3][0] = Queen((3, 0), "white", chessBoard)
+chessBoard[3][7] = Queen((3, 7), "black", chessBoard)
 
 
 background = [[pygame.Surface((width/8, height/8)) for _ in range(8)] for _ in range(8)]
@@ -205,8 +48,6 @@ for row in range(8):
         color = (235, 235, 208) if (row + col) % 2 == 0 else (119, 148, 85)
         background[row][col].fill(color)
         screen.blit(background[row][col], (col * width / 8, row * height / 8))
-
-
 
 
 
---
+++ pieces.py
@@ -0,0 +1,216 @@
+import pygame
+
+class ChessPiece:
+    imageFilePath = ""
+    position = (0, 0)
+    color = ""
+    moveList = []
+
+
+class Pawn(ChessPiece):
+    imageFilePath = "images/pawn.png"
+    
+
+    def __init__(self, position, color, board):
+        self.position = position
+        self.color = color
+        self.board = board
+        self.generateMoveList()
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill((0, 100, 0))  # Placeholder for pawn image
+    
+    def generateMoveList(self):
+        self.moveList = []
+        x, y = self.position
+        direction = 1 if self.color == "white" else -1
+
+        if self.board[x][y + direction] is None:
+            self.moveList.append((x, y + direction))
+
+        if (self.color == "white" and y == 1) or (self.color == "black" and y == 6):
+            self.moveList.append((x, y + 2 * direction))
+        
+        try:
+            if self.board[x - 1][y + direction] is not None:
+                self.moveList.append((x - 1, y + direction))
+        except IndexError:
+            pass
+        try: 
+            if self.board[x + 1][y + direction] is not None:
+                self.moveList.append((x + 1, y + direction))
+        except IndexError:
+            pass
+        
+        ml = self.moveList.copy()
+        for i in ml:
+            if self.board[i[0]][i[1]] is not None:
+                if self.board[i[0]][i[1]].color == self.color:
+                    self.moveList.remove(i)
+        
+
+    def move(self, newPosition):
+        if newPosition in self.moveList:
+            self.position = newPosition
+            self.generateMoveList()
+        else:
+            raise ValueError("Invalid move for Pawn")
+        
+
+
+class Rook(ChessPiece):
+    imageFilePath = "images/rook.png"
+    
+    def __init__(self, position, color, board):
+        self.position = position
+        self.color = color
+        self.board = board
+        self.generateMoveList()
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill((100, 0, 0))  # Placeholder for rook image
+    
+    def generateMoveList(self):
+        self.moveList = []
+        x, y = self.position
+
+        for i in range(x, 8):
+            if i != x:
+                self.moveList.append((i, y))
+                if self.board[i][y] is not None:
+                    break
+        for i in range(x, -1, -1):
+            if i != x:
+                self.moveList.append((i, y))
+                if self.board[i][y] is not None:
+                    break
+        for i in range(y, 8):
+            if i != y:
+                self.moveList.append((x, i))
+                if self.board[x][i] is not None:
+                    break
+        for i in range(y, -1, -1):
+            if i != y:
+                self.moveList.append((x, i))
+                if self.board[x][i] is not None:
+                   break
+
+        ml = self.moveList.copy()
+        for i in ml:
+            if self.board[i[0]][i[1]] is not None:
+                if self.board[i[0]][i[1]].color == self.color:
+                    self.moveList.remove(i)
+
+    def move(self, newPosition):
+        if newPosition in self.moveList:
+            self.position = newPosition
+            self.generateMoveList()
+        else:
+            raise ValueError("Invalid move for Rook")
+        
+
+class Bishop(ChessPiece):
+    imageFilePath = "images/bishop.png"
+    # Implementation similar to Pawn and Rook
+    def __init__(self, position, color, board):
+        self.position = position
+        self.color = color
+        self.board = board
+        self.generateMoveList()
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill((0, 0, 100))  # Placeholder for bishop image
+    def generateMoveList(self):
+        self.moveList = []
+        x, y = self.position
+
+        # Diagonal moves
+        for i in range(1, 8):
+            if x + i < 8 and y + i < 8:
+                self.moveList.append((x + i, y + i))
+                if self.board[x + i][y + i] is not None:
+                    break
+        for i in range(1, 8):
+            if x - i >= 0 and y + i < 8:
+                self.moveList.append((x - i, y + i))
+                if self.board[x - i][y + i] is not None:
+                    break
+        for i in range(1, 8):
+            if x + i < 8 and y - i >= 0:
+                self.moveList.append((x + i, y - i))
+                if self.board[x + i][y - i] is not None:
+                    break
+        for i in range(1, 8):
+            if x - i >= 0 and y - i >= 0:
+                self.moveList.append((x - i, y - i))
+                if self.board[x - i][y - i] is not None:
+                    break
+
+        ml = self.moveList.copy()
+        for i in ml:
+            if self.board[i[0]][i[1]] is not None:
+                if self.board[i[0]][i[1]].color == self.color:
+                    self.moveList.remove(i)
+    def move(self, newPosition):
+        if newPosition in self.moveList:
+            self.position = newPosition
+            self.generateMoveList()
+        else:
+            raise ValueError("Invalid move for Bishop")
+        
+class Knight(ChessPiece):
+    imageFilePath = "images/knight.png"
+    # Implementation similar to Pawn and Rook
+    def __init__(self, position, color, board):
+        self.position = position
+        self.color = color
+        self.board = board
+        self.generateMoveList()
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill((100, 100, 0))  # Placeholder for knight image
+    def generateMoveList(self):
+        self.moveList = []
+        x, y = self.position
+
+        # L-shaped moves
+        potential_moves = [
+            (x + 2, y + 1), (x + 2, y - 1),
+            (x - 2, y + 1), (x - 2, y - 1),
+            (x + 1, y + 2), (x + 1, y - 2),
+            (x - 1, y + 2), (x - 1, y - 2)
+        ]
+        for move in potential_moves:
+            if 0 <= move[0] < 8 and 0 <= move[1] < 8:
+                if self.board[move[0]][move[1]] is None or self.board[move[0]][move[1]].color != self.color:
+                    self.moveList.append(move)
+
+    def move(self, newPosition):
+        if newPosition in self.moveList:
+            self.position = newPosition
+            self.generateMoveList()
+        else:
+            raise ValueError("Invalid move for Knight")
+
+class Queen(ChessPiece):
+    imageFilePath = "images/queen.png"
+    
+    def __init__(self, position, color, board):
+        self.position = position
+        self.color = color
+        self.board = board
+        self.generateMoveList()
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill((0, 100, 100))  # Placeholder for queen image
+    
+    def generateMoveList(self):
+        self.moveList = []
+        # Combine Rook and Bishop move generation
+        rook_like = Rook(self.position, self.color, self.board)
+        rook_like.generateMoveList()
+        bishop_like = Bishop(self.position, self.color, self.board)
+        bishop_like.generateMoveList()
+        self.moveList = rook_like.moveList + bishop_like.moveList
+    
+    def move(self, newPosition):
+        if newPosition in self.moveList:
+            self.position = newPosition
+            self.generateMoveList()
+        else:
+            raise ValueError("Invalid move for Queen")
